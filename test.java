181ce859dcac9eae2b1105d27a03af85c9d992f1
John Simonsen <johnsimonsen@weber.edu>
2019-08-30 17:25:00 -0600

    Update test.java

--- test.java
+++ test.java
@@ -1,11 +1,34 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.io.*;
 
 public class test{
+  private ByteArrayOutputStream TOut;
+  private ByteArrayInputStream TIn;
+  private final PrintStream SOut = System.out;
+  private final InputStream SIn = System.in;
+
+  public void setOutput(){
+    TOut = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(TOut));
+  }
+  public void setInput(String data){
+    TIn = new ByteArrayInputStream(data.getBytes());
+    System.setIn(TIn);
+  }
+  public String getOutput(){
+    return TOut.toString();
+  }
+  public void restoreSystem(){
+    System.setOut(SOut);
+    System.setIn(SIn);
+  }
+
   public static void main(String[] args){
-    LocalDateTime x = LocalDateTime.of(2019,8,30,17,29,30);
+    LocalDateTime x1 = LocalDateTime.of(2019,8,30,17,29,30);
+    LocalDateTime x2 = LocalDateTime.of(2019,8,29,17,29,30);
     DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    System.out.println(x);
-    System.out.println(f.format(x));
+    System.out.println(x1.isBefore(x2));
+
   }
 }
